1e8ed99b832a2c35da433333f487cb433265b51e
Emre Yurtseven <emreyurtseven18@gmail.com>
2022-07-03 01:26:23 +0200

    change dir

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -4,24 +4,13 @@
 
 import * as $0 from "./routes/impressum.tsx";
 import * as $1 from "./routes/index.tsx";
-import * as $$0 from "./islands/About.tsx";
-import * as $$1 from "./islands/Counter.tsx";
-import * as $$2 from "./islands/Divider.tsx";
-import * as $$3 from "./islands/MyFooter.tsx";
-import * as $$4 from "./islands/Welcome.tsx";
 
 const manifest = {
   routes: {
     "./routes/impressum.tsx": $0,
     "./routes/index.tsx": $1,
   },
-  islands: {
-    "./islands/About.tsx": $$0,
-    "./islands/Counter.tsx": $$1,
-    "./islands/Divider.tsx": $$2,
-    "./islands/MyFooter.tsx": $$3,
-    "./islands/Welcome.tsx": $$4,
-  },
+  islands: {},
   baseUrl: import.meta.url,
 };
 
