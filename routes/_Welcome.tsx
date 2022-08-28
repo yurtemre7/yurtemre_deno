626ee913faf9eebdc9ee790e907d1ddcc5a2ca15
Emre Yurtseven <emreyurtseven18@gmail.com>
2022-08-28 21:28:22 +0200

    improve type

--- routes/_Welcome.tsx
+++ routes/_Welcome.tsx
@@ -2,7 +2,11 @@
 import { h } from "preact";
 import { tw } from "@twind";
 
-export default function Welcome({ joke }: string) {
+interface JokeProp {
+  joke: string;
+}
+
+export default function Welcome({ joke }: JokeProp) {
   return (
     <div class={tw`dark:bg-black m-10`}>
       <div
