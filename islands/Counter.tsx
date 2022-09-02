0f42629fb18a57dfdec0f82ca4d86001cacda203
Emre Yurtseven <emreyurtseven18@gmail.com>
2022-09-02 14:50:54 +0200

    better text

--- islands/Counter.tsx
+++ islands/Counter.tsx
@@ -12,10 +12,12 @@ interface CounterProps {
 export default function Counter(props: CounterProps) {
   const [count, setCount] = useState(props.start);
   return (
-    <div class={tw`flex gap-2 w-full`}>
-      <p class={tw`flex-grow-1 font-bold text-xl`}>{count}</p>
-      <Button onClick={() => setCount(count - 1)}>-1</Button>
-      <Button onClick={() => setCount(count + 1)}>+1</Button>
+    <div class={tw`h-screen text-center flex items-center justify-center`}>
+      <div class={tw`flex gap-2 dark:text-gray-500`}>
+        <p class={tw`flex-grow-1 font-bold text-xl`}>{count}</p>
+        <Button onClick={() => setCount(count - 1)}>-1</Button>
+        <Button onClick={() => setCount(count + 1)}>+1</Button>
+      </div>
     </div>
   );
 }
