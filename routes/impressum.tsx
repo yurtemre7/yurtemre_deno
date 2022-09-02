12699b6e92beff7ce19fefeafa73f4bca201b74b
Emre Yurtseven <emreyurtseven18@gmail.com>
2022-09-02 14:51:00 +0200

    better impressum

--- routes/impressum.tsx
+++ routes/impressum.tsx
@@ -4,32 +4,38 @@ import { tw } from "@twind";
 
 export default function Impressum() {
   return (
-    <div class={tw`dark:bg-black h-screen text-center flex items-center justify-center`}>
-      <div class={tw`p-5 dark:text-white`}>
-        <head>
-          <title>yurtemre.de | Impressum</title>
-        </head>
-        <p class={tw`text-left text-2xl font-bold`}>
-          IMPRESSUM
+    <div
+      class={tw`dark:bg-black h-screen text-center flex items-center`}
+    >
+      <head>
+        <title>yurtemre.de | Impressum</title>
+      </head>
+      <div class={tw`flex-1 text-white`}>
+        <h1 class={tw`text-3xl`}>Impressum</h1>
+        <p class={tw`text-lg`}>
+          <p>yurtemre.de</p>
+          <br />
         </p>
-        <div class={tw`m-4`} />
-        <p class={tw`text-left text-xl font-bold`}>
+      </div>
+
+      <div class={tw`p-5 dark:text-white flex-1`}>
+        <p class={tw`text-xl font-bold`}>
           Angaben gemäß § 5 TMG
         </p>
-        <p class={tw`text-left`}>
+        <p>
           Emre Yurtseven
         </p>
-        <p class={tw`text-left`}>
+        <p>
           Gradestraße 30, <br /> 12347 Berlin, Deutschland
         </p>
         <div class={tw`m-4`} />
-        <p class={tw`text-left text-xl font-bold`}>
+        <p class={tw`text-xl font-bold`}>
           Kontakt
         </p>
-        <p class={tw`text-left`}>
+        <p>
           Telefon: +49 177 921 43 52
         </p>
-        <p class={tw`text-left`}>
+        <p>
           E-Mail:
           <a href="mailto:emreyurtseven18@gmail.com">
             {" emreyurtseven18@gmail.com"}
