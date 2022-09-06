8691bc0569ddfdba3e459e32fc784298d28320cc
Emre Yurtseven <emreyurtseven18@gmail.com>
2022-09-06 22:44:18 +0200

    island

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -6,6 +6,7 @@ import * as $0 from "./routes/gvm.tsx";
 import * as $1 from "./routes/impressum.tsx";
 import * as $2 from "./routes/index.tsx";
 import * as $$0 from "./islands/Counter.tsx";
+import * as $$1 from "./islands/TextBox.tsx";
 
 const manifest = {
   routes: {
@@ -15,6 +16,7 @@ const manifest = {
   },
   islands: {
     "./islands/Counter.tsx": $$0,
+    "./islands/TextBox.tsx": $$1,
   },
   baseUrl: import.meta.url,
 };
