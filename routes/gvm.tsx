e2ea88b4688ed7ab1714c4fede18b3dd6e65f0d7
Emre Yurtseven <emreyurtseven18@gmail.com>
2022-09-09 14:19:02 +0200

    error

--- routes/gvm.tsx
+++ routes/gvm.tsx
@@ -25,33 +25,29 @@ export const handler: Handlers<InitialData> = {
     const assembly = formData.get("assembly") as string;
     let result = "";
 
-    let succeed = false;
-
     if (assembly.length > 0) {
-      // execute shell on server
-      const p = Deno.run({
-        // cmd: ["cmd", "/c", "EML", assembly], // windows
-        cmd: ["echo", assembly, input], // linux
-        // cmd: ["gvm", "j++", "otf", assembly, input], // joshua = bottleneck
-        stdout: "piped",
-      });
-
-      setTimeout(() => {
-        // if process is still running, kill it
-        if (!succeed) {
-          p.kill("SIGTERM");
+      const body = new FormData();
+      body.append("assembly", assembly);
+      body.append("input", input);
+      try {
+        const res = await fetch("http://server.yurtemre.de:6060/gvm", {
+          method: "POST",
+          body,
+        }).catch((err) => {
+          console.log(err);
+          result = "Error: " + err;
+          return err;
+        });
+        // check if res is of type Response
+        if (res instanceof Response) {
+          result = await res.text();
         }
-      }, 1000 * 8);
-
-      const stdout = await p.output();
-      succeed = true;
-
-      p.close();
-      const txt = new TextDecoder().decode(stdout);
-      // console.log("result", txt);
-
-      result = txt;
+      } catch (error) {
+        result = "Error: " + error;
+      }
     }
+
+    console.log(result);
 
     const data: InitialData = {
       code: assembly,
