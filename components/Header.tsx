31be91fd36c16bd461676022d32a72cf96142c5a
Emre Yurtseven <emreyurtseven18@gmail.com>
2022-09-28 11:48:39 +0300

    fix title white mode non visable

--- components/Header.tsx
+++ components/Header.tsx
@@ -13,7 +13,7 @@ export default function Header() {
           <path d="M5.854 4.854a.5.5 0 1 0-.708-.708l-3.5 3.5a.5.5 0 0 0 0 .708l3.5 3.5a.5.5 0 0 0 .708-.708L2.707 8l3.147-3.146zm4.292 0a.5.5 0 0 1 .708-.708l3.5 3.5a.5.5 0 0 1 0 .708l-3.5 3.5a.5.5 0 0 1-.708-.708L13.293 8l-3.147-3.146z" />
         </svg>
       </a>
-      <div class="dark:bg-black text-white flex-auto">
+      <div class="dark:text-white text-black flex-auto">
         <h1 class="text-4xl font-bold text-center">yurtemre.de</h1>
         <p class="text-xl font-bold text-center">by Emre Yurtseven</p>
       </div>
