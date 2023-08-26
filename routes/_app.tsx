b16ed370c94a6b45c859db71332c8cdffe3e357c
Emre Yurtseven <emreyurtseven18@gmail.com>
2023-08-26 11:58:28 +0200

    add better app wrapper

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,9 +1,17 @@
 import { AppProps } from "$fresh/server.ts";
 
-export default function App({ Component }: AppProps) {
+export default function App({ Component, state }: AppProps) {
+  // do something with state here
   return (
-    <>
-      <Component />
-    </>
+    <html>
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>yurtemre.de</title>
+      </head>
+      <body>
+        <Component />
+      </body>
+    </html>
   );
 }
