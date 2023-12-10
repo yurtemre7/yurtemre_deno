82c5b84e230bf2f80f80fc2cb190b99a7afb89c3
Emre Yurtseven <emreyurtseven18@gmail.com>
2023-12-10 15:21:20 +0100

    push

--- islands/NewYear.tsx
+++ islands/NewYear.tsx
@@ -1,11 +1,11 @@
 import { useEffect, useState } from "preact/hooks";
 
 interface TimeLeft {
-    days: number;
-    hours: number;
-    minutes: number;
-    seconds: number;
-    }
+  days: number;
+  hours: number;
+  minutes: number;
+  seconds: number;
+}
 
 export default function NewYearCountdown() {
   const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
@@ -20,11 +20,11 @@ export default function NewYearCountdown() {
   function calculateTimeLeft() {
     const year = new Date().getFullYear();
     const difference = +new Date(`${year + 1}-01-01`) - +new Date();
-    let timeLeft : TimeLeft = {
+    let timeLeft: TimeLeft = {
       days: 0,
       hours: 0,
       minutes: 0,
-      seconds: 0
+      seconds: 0,
     };
 
     if (difference > 0) {
@@ -39,17 +39,14 @@ export default function NewYearCountdown() {
     return timeLeft;
   }
 
+  const currentYear = new Date().getFullYear();
+
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-4xl mb-8">Zeit bis zum Neujahr</h1>
       <div className="relative w-64 h-64">
-        <div className="absolute top-0 left-0 w-full h-full rounded-full border-8 border-gray-200" />
-        <div
-          className="absolute top-0 left-0 w-full h-full rounded-full border-8 border-blue-500"
-          style={{ clipPath: "inset(0 50% 0 0)" }}
-        />
         <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center text-2xl">
-          {`${timeLeft.days}d ${timeLeft.hours}h ${timeLeft.minutes}m ${timeLeft.seconds}s`}
+        {currentYear >= 2024 ? 'Frohes neues Jahr 🎆' : `${timeLeft.days}d ${timeLeft.hours}h ${timeLeft.minutes}m ${timeLeft.seconds}s`}
         </div>
       </div>
     </div>
