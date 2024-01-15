f36817e7b420425701e9bc2f17395967e70983d0
Emre Yurtseven <emreyurtseven18@gmail.com>
2024-01-15 19:52:53 +0100

    add blue background to html

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,8 +1,7 @@
 import { type PageProps } from "$fresh/server.ts";
-import { Head } from "$fresh/runtime.ts";
 export default function App({ Component }: PageProps) {
   return (
-    <html>
+    <html className="bg-blue-500">
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
