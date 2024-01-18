6cf4bbdc10209037b0f8135cacad1267ca7e154a
Emre Yurtseven <emreyurtseven18@gmail.com>
2024-01-18 20:26:40 +0100

    fix

--- routes/index.tsx
+++ routes/index.tsx
@@ -56,7 +56,7 @@ export default function Home() {
             <path d="M6 0H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3H2a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Zm10 0h-4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3h-1a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Z" />
           </svg>
           <blockquote>
-            <p className="text-2xl italic font-medium text-centertext-white">"iiiiiiieeeeeehhh Javascript !!!"</p>
+            <p className="text-2xl italic font-medium text-center text-white">"iiiiiiieeeeeehhh Javascript !!!"</p>
           </blockquote>
           <figcaption className="flex items-center justify-center mt-6 space-x-3 rtl:space-x-reverse">
             <div className="flex items-center divide-x-2 rtl:divide-x-reverse divide-gray-400">
