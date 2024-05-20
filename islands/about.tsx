ca9b9c05c65dc4959faf9dd22f1b02a05dff93e8
Emre Yurtseven <emreyurtseven18@gmail.com>
2024-05-20 15:09:08 +0200

    improve about me

--- islands/about.tsx
+++ islands/about.tsx
@@ -1,23 +1,9 @@
-import { useSignal } from "@preact/signals";
-
-interface App {
-    id: number;
-    name: string;
-    description: string;
-}
+import MyApps from "./my_apps.tsx";
 
 export default function AboutMe() {
     const diff = new Date().getTime() - new Date("2020-10-01").getTime();
     const days = Math.ceil(diff / (1000 * 60 * 60 * 24));
     const semester = Math.ceil(days / (365 / 2));
-
-    const selectedApp = useSignal<App | null>(null);
-
-    const apps = [
-        { id: 1, name: 'WoAuto', description: 'Mit WoAuto verlierst du nie wieder dein Auto, Fahrrad oder sonst was du parken kannst!' },
-        { id: 2, name: 'SNP', description: 'SNP gibt es zwar noch nicht, aber wenn du auf eine Revolution von HTTP wartest, dann bist du hier genau richtig!' },
-        { id: 3, name: 'Deez', description: 'Nuts :D' },
-    ];
 
     return (
         <div className="min-h-screen flex items-center justify-center">
@@ -46,35 +32,10 @@ export default function AboutMe() {
                         Flutter
                     </a>.
                 </p>
-                {/* <p className="text-xl">
-                    Hier findest du all meine Projekte:{" "}
-                    <a
-                        className="hover:underline font-bold"
-                        href="https://github.com/yurtemre7"
-                    >
-                        GitHub
-                    </a>
-                </p> */}
-                <div className="mt-12" />
-                <div className="flex justify-center">
-                    <div className="w-1/2">
-                        <h3 className="text-3xl font-bold mb-4">Best Apps</h3>
-                        <ul>
-                            {apps.map(app => (
-                                <li key={app.id} className="underline hover:text-blue-200 cursor-pointer" onClick={() => selectedApp.value = app}>{app.name}</li>
-                            ))}
-                        </ul>
-                    </div>
-                    <div className="w-1/2">
-                        {selectedApp.value && (
-                            <>
-                                <h3 className="text-3xl font-bold  mb-4">{selectedApp.value.name}</h3>
-                                <p className="">{selectedApp.value.description}</p>
-                            </>
-                        )}
-                    </div>
-                </div>
+                <div className="mt-24" />
+                <MyApps />
             </div>
+
         </div>
     );
 }
