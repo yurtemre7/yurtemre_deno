4e48d370050a7b72b57e8c5660c76b05b3d856fc
Emre Yurtseven <emreyurtseven18@gmail.com>
2024-07-29 21:32:31 +0200

    improve iPhone safari colors

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -5,6 +5,9 @@ export default function App({ Component }: PageProps) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+
         <title>yurtemre.de</title>
         <link rel="stylesheet" href="/styles.css" />
       </head>
