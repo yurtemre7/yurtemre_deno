9afa5d37072e9d3bc32847b236b8b13e1405e633
Emre Yurtseven <emreyurtseven18@gmail.com>
2024-08-02 20:55:57 +0200

    added playstore link to paren

--- components/paren.tsx
+++ components/paren.tsx
@@ -1,5 +1,6 @@
 const parenURL = "https://yurtemre7.github.io/paren/";
-const parenDownloadURL = "https://apps.apple.com/us/app/paren/id6578395712";
+const parenAppleDownloadURL = "https://apps.apple.com/us/app/paren/id6578395712";
+const parenGoogleDownloadURL = "https://play.google.com/store/apps/details?id=de.emredev.paren";
 
 export default function Paren() {
     return (
@@ -12,11 +13,22 @@ export default function Paren() {
             </div>
             <div className="text-center items-center w-1/2 my-10">
                 <h2 className="text-4xl font-bold text-white mb-8">Download Par円👇</h2>
-                <div className="flex justify-center">
-                    <a href={parenDownloadURL}>
+                <div className="flex justify-center sm:flex-row flex-col m-8">
+                    <a href={parenAppleDownloadURL}>
                         <img
+                            width={215}
+                            height={75}
                             src="/appstore_dl.svg"
                             alt="Download Paren in the Apple AppStore"
+                        />
+                    </a>
+                    <div className="m-4"></div>
+                    <a href={parenGoogleDownloadURL}>
+                        <img
+                            width={240}
+                            height={80}
+                            src="/playstore_dl.png"
+                            alt="Download Paren in the Google Play Store"
                         />
                     </a>
                 </div>
