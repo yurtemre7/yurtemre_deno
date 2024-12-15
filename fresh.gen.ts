dc58bb1225c7972b4f8b29d64fde1365796060c2
Emre Yurtseven <emreyurtseven18@gmail.com>
2024-12-15 20:21:15 +0100

    remove files

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -4,7 +4,6 @@
 
 import * as $_404 from "./routes/_404.tsx";
 import * as $_app from "./routes/_app.tsx";
-import * as $_index from "./routes/_index.tsx";
 import * as $datenschutz from "./routes/datenschutz.tsx";
 import * as $deeplink from "./routes/deeplink.tsx";
 import * as $fasting from "./routes/fasting.tsx";
@@ -13,16 +12,11 @@ import * as $index from "./routes/index.tsx";
 import * as $paren from "./routes/paren.tsx";
 import * as $sync from "./routes/sync.tsx";
 import * as $CountdownClock from "./islands/CountdownClock.tsx";
-import * as $DateCountdown from "./islands/DateCountdown.tsx";
 import * as $DeeplinkButton from "./islands/DeeplinkButton.tsx";
 import * as $SyncButton from "./islands/SyncButton.tsx";
-import * as $about from "./islands/about.tsx";
-import * as $ai_chat from "./islands/ai_chat.tsx";
 import * as $fasting_widget from "./islands/fasting_widget.tsx";
 import * as $index_1 from "./islands/index.tsx";
-import * as $my_apps from "./islands/my_apps.tsx";
 import * as $paren_flutter from "./islands/paren_flutter.tsx";
-import * as $smoothscroll_btn from "./islands/smoothscroll_btn.tsx";
 import * as $snowfall from "./islands/snowfall.tsx";
 import type { Manifest } from "$fresh/server.ts";
 
@@ -30,7 +24,6 @@ const manifest = {
   routes: {
     "./routes/_404.tsx": $_404,
     "./routes/_app.tsx": $_app,
-    "./routes/_index.tsx": $_index,
     "./routes/datenschutz.tsx": $datenschutz,
     "./routes/deeplink.tsx": $deeplink,
     "./routes/fasting.tsx": $fasting,
@@ -41,16 +34,11 @@ const manifest = {
   },
   islands: {
     "./islands/CountdownClock.tsx": $CountdownClock,
-    "./islands/DateCountdown.tsx": $DateCountdown,
     "./islands/DeeplinkButton.tsx": $DeeplinkButton,
     "./islands/SyncButton.tsx": $SyncButton,
-    "./islands/about.tsx": $about,
-    "./islands/ai_chat.tsx": $ai_chat,
     "./islands/fasting_widget.tsx": $fasting_widget,
     "./islands/index.tsx": $index_1,
-    "./islands/my_apps.tsx": $my_apps,
     "./islands/paren_flutter.tsx": $paren_flutter,
-    "./islands/smoothscroll_btn.tsx": $smoothscroll_btn,
     "./islands/snowfall.tsx": $snowfall,
   },
   baseUrl: import.meta.url,
