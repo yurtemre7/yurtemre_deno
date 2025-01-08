a95b34551c0a796d792806eda7886a6ffea8f1d2
Emre Yurtseven <emreyurtseven18@gmail.com>
2025-01-08 19:51:22 +0100

    fix footer color

--- components/my_footer.tsx
+++ components/my_footer.tsx
@@ -1,6 +1,6 @@
 export default function MyFooter() {
     return (
-        <footer className="bg-blue-800 text-white py-4 px-6 md:flex md:justify-between flex-col">
+        <footer className="py-4 px-6 md:flex md:justify-between flex-col">
             <div className="md:justify-between md:flex md:items-center flex-col md:flex-row">
                 <div className="text-center md:text-left">
                     <p>© 2020 - {new Date().getFullYear()} <a className="hover:underline" href="/">yurtemre.de</a></p>
@@ -24,7 +24,6 @@ export default function MyFooter() {
                     </div>
                 </div>
                 <div className="mt-4 md:mt-0 text-center md:text-right">
-                    <a href="/fasting" className="hover:underline hover:text-blue-200 mr-4">Fasting</a>
                     <a href="/impressum" className="hover:underline hover:text-blue-200 mr-4">Impressum</a>
                     <a href="/datenschutz" className="hover:underline hover:text-blue-200">Datenschutz</a>
                 </div>
