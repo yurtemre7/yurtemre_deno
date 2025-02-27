24dfe8b80378237bc901dfb27dfe1cca205820a7
Emre Yurtseven <emreyurtseven18@gmail.com>
2025-02-28 00:14:18 +0100

    fix: enhance date formatting and improve layout for fasting days display

--- routes/fasting.tsx
+++ routes/fasting.tsx
@@ -65,7 +65,7 @@ export default function Fasting() {
         // console.log(tomorrow);
         const fastingBegin = fastingDates.get(formatterToday.format(tomorrow))!.begin || Date.now();
         const fastingEnd = fastingDates.get(formatterToday.format(tomorrow))!.end || Date.now();
-        const longFormatter = new Intl.DateTimeFormat('de-DE', { month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' });
+        const longFormatter = new Intl.DateTimeFormat('de-DE', { weekday: 'long', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' });
         const shortFormatter = new Intl.DateTimeFormat('de-DE', { hour: '2-digit', minute: '2-digit' });
         const fastingStr = longFormatter.format(fastingBegin) + ' Uhr - ' + shortFormatter.format(fastingEnd) + ' Uhr';
         fastingDays.value.push(fastingStr);
@@ -104,11 +104,11 @@ export default function Fasting() {
                                             Die restlichen Tage ({fastingDays.value.length} Tag{fastingDays.value.length > 1 ? "e" : ""}) vom Ramadan sind wie folgt:
                                         </p>
                                         <div className="mt-5" />
-                                        <div className="text-center overflow-auto h-96 px-6 divide-y-2 divide-opacity-20 divide-white">
+                                        <div className="text-center overflow-auto h-full px-6 divide-y-2 divide-opacity-20 divide-white">
                                             {
                                                 fastingDays.value.map(
-                                                    (day: string, i: number) => (
-                                                        <p className="text-l py-2 hover:underline">{i + 1}: {day}</p>
+                                                    (day: string, _) => (
+                                                        <p className="text-l py-3 hover:underline">{day}</p>
                                                     )
                                                 )
                                             }
@@ -135,14 +135,14 @@ export default function Fasting() {
         tomorrow.setDate(tomorrow.getDate() + i);
         const fastingBegin = fastingDates.get(formatterToday.format(tomorrow))!.begin || Date.now();
         const fastingEnd = fastingDates.get(formatterToday.format(tomorrow))!.end || Date.now();
-        const longFormatter = new Intl.DateTimeFormat('de-DE', { month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' });
+        const longFormatter = new Intl.DateTimeFormat('de-DE', { weekday: 'long', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' });
         const shortFormatter = new Intl.DateTimeFormat('de-DE', { hour: '2-digit', minute: '2-digit' });
         const fastingStr = longFormatter.format(fastingBegin) + ' Uhr - ' + shortFormatter.format(fastingEnd) + ' Uhr';
         daysAfterFasting.value.push(fastingStr);
     }
 
     return (
-        <html className="bg-blue-500 bg-gradient-to-r from-indigo-600 to-blue-600 text-white">
+        <html className="bg-gray-100 text-gray-900 dark:bg-gray-900 dark:text-gray-100">
             <head>
                 <title>yurtemre.de | fasting</title>
             </head>
@@ -182,7 +182,7 @@ export default function Fasting() {
                                             Die restlichen Tage ({daysAfterFasting.value.length} Tag{daysAfterFasting.value.length > 1 ? "e" : ""}) vom Ramadan sind wie folgt:
                                         </p>
                                         <div className="mt-5" />
-                                        <div className="text-center overflow-auto h-96 px-6 divide-y-2 divide-opacity-20 divide-white">
+                                        <div className="text-center overflow-auto h-full px-6 divide-y-2 divide-opacity-20 divide-white">
                                             {
                                                 daysAfterFasting.value.map(
                                                     (day: string, i: number) => (
