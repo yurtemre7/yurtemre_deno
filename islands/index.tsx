1b9e0da881db5b5c9f0091853919fec74b84a91b
Emre Yurtseven <emreyurtseven18@gmail.com>
2025-03-03 23:02:22 +0100

    remove border

--- islands/index.tsx
+++ islands/index.tsx
@@ -195,7 +195,7 @@ export default function Home({ wotd, lang }: InitialData) {
       
 
       {/* Countdown Timers - Improved layout */}
-      <section className="py-8 bg-white dark:bg-gray-900 border-t border-gray-200 dark:border-gray-700">
+      <section className="py-4 bg-white dark:bg-gray-900">
         <div className="container mx-auto px-4">
           <div className="grid grid-cols-1 sm:grid-cols-3 gap-6">
             {t.countdownLabels.map((label, index) => (
