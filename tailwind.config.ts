c835066b3b0f9ffe8fc676057dd0dde27906f24b
Emre Yurtseven <emreyurtseven18@gmail.com>
2025-06-22 17:53:32 +0200

    feat: add dark mode and extend font family in Tailwind configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,15 @@
 import { type Config } from "tailwindcss";
 
 export default {
+  darkMode: "class",
   content: [
     "{routes,islands,components}/**/*.{ts,tsx}",
   ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "ui-sans-serif", "system-ui"],
+      },
+    },
+  },
 } satisfies Config;
