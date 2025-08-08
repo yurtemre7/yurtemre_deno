b584bf2056592412a5615d65e9f7ca9743b82fb5
Emre Yurtseven <emreyurtseven18@gmail.com>
2025-08-09 00:08:51 +0200

    improve head

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -7,21 +7,29 @@ export default function App(props: PageProps) {
       className="bg-[#0A0F1E] text-[#E2E8F0] antialiased transition-colors duration-200"
     >
       <head>
+        {/* Character encoding should be first */}
         <meta charset="utf-8" />
+        
+        {/* Viewport and mobile settings */}
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta
           name="apple-mobile-web-app-status-bar-style"
           content="black-translucent"
         />
+        
+        {/* Title and description for SEO */}
+        <title>{props.url.host}</title>
         <meta
           name="description"
           content="Portfolio website of Emre Yurtseven, the most krass app developer."
         />
-
-        <title>{props.url.host}</title>
-        <meta name="theme-color" content="#121212" />
+        
+        {/* Styling */}
         <link rel="stylesheet" href="/styles.css" />
+        
+        {/* Theme and other meta */}
+        <meta name="theme-color" content="#121212" />
       </head>
       <body className="min-h-screen antialiased">
         <props.Component />
